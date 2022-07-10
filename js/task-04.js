e5034a2f69969b07b239b378a5dc371ee33b8f23
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-10 21:03:30 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,29 @@
-const value = document.querySelector('#value');
-const counterValue = 0;
+const counterValue = document.querySelector('#value');
 const btn = document.querySelector('#counter')
-console.log(btn.firstElementChild)
-console.log(btn.lastElementChild)
+console.log(counterValue.textContent)
 
 
-btn.firstElementChild.addEventListener("click", () => {
-    console.log("Button was clicked");
-  });
+
+// btn.firstElementChild.addEventListener("click", (value) => {
+//     // console.log("-1");
+//     value - 1;
+//   });
+//   btn.lastElementChild.addEventListener("click", () => {
+//     console.log("+1");
+//   });
+
+
+
+  // const button = document.querySelector(".my-button");
+
+const minus = () => {
+  counterValue.innerHTML -= 1;
+  console.log("-1");
+};
+btn.firstElementChild.addEventListener("click", minus);
+
+const plus = () => {
+  counterValue.innerHTML += 1;
+  console.log("+1");
+};
+btn.lastElementChild.addEventListener("click", plus);
