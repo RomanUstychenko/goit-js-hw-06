e44cd20186087c4d0ffb72edfab8a07cfb169d72
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-10 15:13:14 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,9 @@
 const value = document.querySelector('#value');
 const counterValue = 0;
 const btn = document.querySelector('#counter')
-const firstBtn = 
 console.log(btn.firstElementChild)
 console.log(btn.lastElementChild)
-// const firstBtn = btn.childNodes[3];
-// console.log(firstBtn)
+
 
 btn.firstElementChild.addEventListener("click", () => {
     console.log("Button was clicked");
