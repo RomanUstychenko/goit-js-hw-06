6b2281634b19ae00338ae13ff0f42d8eb66066cb
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-18 18:34:33 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,23 @@
 const form = document.querySelector('.login-form');
 
 form.addEventListener("submit", handleSubmit);
+
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, password }
   } = event.currentTarget;
-
   if (email.value === "" || 
-  password.value === "" || 
-  password.value.indexOf(' ') > -1) {
+  password.value === "" 
+  || password.value.indexOf(' ') > -1) 
+  {
     alert ("Всі поля повинні бути заповнені без пробілів!");
     return;
   }
-
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+    const formData = {
+    email: email.value,
+    password: password.value
+  }
+  console.log(formData)
   event.currentTarget.reset();
 }
